51d853ebb263d0e68eed231df96d42496dddcffb
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-02-14 11:27:02 +0100

    FIRST COMMIT

---
+++ js/main.js
@@ -0,0 +1,198 @@
+//TODO Milestone 1
+// Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e
+// dall’interlocutore (bianco) assegnando due classi CSS
+// Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare
+// nome e immagine di ogni contatto
+
+//TODO Milestone 2
+// ● Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i
+// messaggi relativi al contatto attivo all’interno del pannello della conversazione
+// ● Click sul contatto mostra la conversazione del contatto cliccato
+
+//TODO Milestone 3
+// ● Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando
+// “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+// ● Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà
+// un “ok” come risposta, che apparirà dopo 1 secondo.
+
+//TODO Milestone 4
+// ● Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i
+// contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo
+// “mar” rimangono solo Marco e Martina)
+
+//TODO Milestone 5 - opzionale
+// ● Cancella messaggio: cliccando sul messaggio appare un menu a tendina che
+// permette di cancellare il messaggio selezionato
+
+const app = Vue.createApp({
+  data() {
+    return {
+      contacts: [
+        {
+          name: "Michele",
+          avatar: "_1",
+          visible: true,
+          messages: [
+            {
+              date: "10/01/2020 15:30:55",
+              MText: "Hai portato a spasso il cane?",
+              status: "sent",
+            },
+            {
+              date: "10/01/2020 15:50:00",
+              MText: "Ricordati di stendere i panni",
+              status: "sent",
+            },
+            {
+              date: "10/01/2020 16:15:22",
+              MText: "Tutto fatto!",
+              status: "received",
+            },
+          ],
+        },
+        {
+          name: "Fabio",
+          avatar: "_2",
+          visible: true,
+          messages: [
+            {
+              date: "20/03/2020 16:30:00",
+              MText: "Ciao come stai?",
+              status: "sent",
+            },
+            {
+              date: "20/03/2020 16:30:55",
+              MText: "Bene grazie! Stasera ci vediamo?",
+              status: "received",
+            },
+            {
+              date: "20/03/2020 16:35:00",
+              MText: "Mi piacerebbe ma devo andare a fare la spesa.",
+              status: "sent",
+            },
+          ],
+        },
+        {
+          name: "Samuele",
+          avatar: "_3",
+          visible: true,
+
+          messages: [
+            {
+              date: "28/03/2020 10:10:40",
+              MText: "La Marianna va in campagna",
+              status: "received",
+            },
+            {
+              date: "28/03/2020 10:20:10",
+              MText: "Sicuro di non aver sbagliato chat?",
+              status: "sent",
+            },
+            {
+              date: "28/03/2020 16:15:22",
+              MText: "Ah scusa!",
+              status: "received",
+            },
+          ],
+        },
+        {
+          name: "Alessandro B.",
+          avatar: "_4",
+          visible: true,
+          messages: [
+            {
+              date: "10/01/2020 15:30:55",
+              MText: "Lo sai che ha aperto una nuova pizzeria?",
+              status: "sent",
+            },
+            {
+              date: "10/01/2020 15:50:00",
+              MText: "Si, ma preferirei andare al cinema",
+              status: "received",
+            },
+          ],
+        },
+        {
+          name: "Alessandro L.",
+          avatar: "_5",
+          visible: true,
+          messages: [
+            {
+              date: "10/01/2020 15:30:55",
+              MText: "Ricordati di chiamare la nonna",
+              status: "sent",
+            },
+            {
+              date: "10/01/2020 15:50:00",
+              MText: "Va bene, stasera la sento",
+              status: "received",
+            },
+          ],
+        },
+        {
+          name: "Claudia",
+          avatar: "_6",
+          visible: true,
+          messages: [
+            {
+              date: "10/01/2020 15:30:55",
+              MText: "Ciao Claudia, hai novità?",
+              status: "sent",
+            },
+            {
+              date: "10/01/2020 15:50:00",
+              MText: "Non ancora",
+              status: "received",
+            },
+            {
+              date: "10/01/2020 15:51:00",
+              MText: "Nessuna nuova, buona nuova",
+              status: "sent",
+            },
+          ],
+        },
+        {
+          name: "Federico",
+          avatar: "_7",
+          visible: true,
+          messages: [
+            {
+              date: "10/01/2020 15:30:55",
+              MText: "Fai gli auguri a Martina che è il suo compleanno!",
+              status: "sent",
+            },
+            {
+              date: "10/01/2020 15:50:00",
+              MText: "Grazie per avermelo ricordato, le scrivo subito!",
+              status: "received",
+            },
+          ],
+        },
+        {
+          name: "Davide",
+          avatar: "_8",
+          visible: true,
+          messages: [
+            {
+              date: "10/01/2020 15:30:55",
+              MText: "Ciao, andiamo a mangiare la pizza stasera?",
+              status: "received",
+            },
+            {
+              date: "10/01/2020 15:50:00",
+              MText: "No, l'ho già mangiata ieri, ordiniamo sushi!",
+              status: "sent",
+            },
+            {
+              date: "10/01/2020 15:51:00",
+              MText: "OK!!",
+              status: "received",
+            },
+          ],
+        },
+      ],
+    };
+  },
+  methods: {},
+});
+app.mount("#root");
