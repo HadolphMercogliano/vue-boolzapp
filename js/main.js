42dd13fd48bdce892eedb45d84d92ab67712900c
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-02-15 16:59:04 +0100

    Milestone 2 end + Mil 3 WIP

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@
 // * Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e da dall’interlocutore (bianco) assegnando due classi CSS
 // * Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
 
-//TODO Milestone 2
-// ● Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
-// ● Click sul contatto mostra la conversazione del contatto cliccato
+// * Milestone 2
+// * Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+// * Click sul contatto mostra la conversazione del contatto cliccato
 
 //TODO Milestone 3
-// ● Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando
+// * Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando
 // “enter” il testo viene aggiunto al thread sopra, come messaggio verde
 // ● Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà
 // un “ok” come risposta, che apparirà dopo 1 secondo.
@@ -186,10 +186,26 @@ const app = Vue.createApp({
           ],
         },
       ],
-      activeSlide: 2,
-      message: "ciao",
+      ActiveContact: 0,
+      newMessage: "ciao",
     };
   },
-  methods: {},
+  methods: {
+    switchToContact(index) {
+      //  alert("hai cliccato l immagine n " + index);
+      this.ActiveContact = index;
+    },
+    addMessage(index) {
+      // console.log("sono l'add task");
+      const newMessage = {
+        date: "now",
+        text: this.newMessage,
+        status: "sent",
+      };
+      this.contacts[index].messages.push(newMessage);
+      // console.log(this.tasks);
+      this.newMessage = "";
+    },
+  },
 });
 app.mount("#root");
